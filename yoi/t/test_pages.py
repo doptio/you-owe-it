df6968be0b991a3c6d844fdfdeb03148a2e29879
Sune Kirkeby <mig@ibofobi.dk>
2012-04-13 22:08:29 +0200

    First ever test. Yay \o/

---
+++ yoi/t/test_pages.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals, division
+
+from werkzeug.test import Client
+from werkzeug.wrappers import BaseResponse
+
+from dweeb.testing import setup_module, assert_eq
+
+from yoi.pages import app
+
+def test_index():
+    client = Client(app, BaseResponse)
+    resp = client.get('/')
+    assert_eq(resp.status_code, 200)
