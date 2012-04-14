a0b40a5f0c41f291ce203a46a4fc60358187ff5d
Sune Kirkeby <mig@ibofobi.dk>
2012-04-14 11:14:54 +0200

    Use wrap_application helper.

--- yoi/site.py
+++ yoi/site.py
@@ -1,17 +1,11 @@
 from __future__ import unicode_literals, division
 
-from dweeb.middleware import gzipper, loggy
-from dweeb.config import use_debugger, static_files
+from dweeb.middleware import wrap_application
+from dweeb.resources import dweeb_resources
 
 from yoi.pages import app
 
-app = gzipper(app)
-app = loggy(app)
+app = wrap_application(app)
 
-if use_debugger:
-    from werkzeug.debug import DebuggedApplication
-    app = DebuggedApplication(app, evalex=True)
-
-if static_files:
-    from werkzeug.wsgi import SharedDataMiddleware
-    app = SharedDataMiddleware(app, static_files)
+resources = {}
+resources.update(dweeb_resources)
