f1a17c907a34c8a461e71ef73ff2e92b7f2d4226
Sune Kirkeby <mig@ibofobi.dk>
2012-04-14 11:15:19 +0200

    Server static resources.

--- yoi/pages.py
+++ yoi/pages.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, division
 from selector import Selector
 
 from dweeb.framework import page, with_database
+from dweeb.resources import static_app
 from dweeb.template import with_template
 
 @page
@@ -11,4 +12,5 @@ def get_index(request):
     pass
 
 app = Selector()
+app.add('/static/|', GET=static_app, HEAD=static_app)
 app.add('/', GET=get_index)
