d1b8b2a52edf0145c787d9027e98e943b7d8cc32
Sune Kirkeby <mig@ibofobi.dk>
2012-04-19 21:31:28 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='YoiOweIt',
+    version='0.0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # FIXME - read requirements.txt!
+    ],
+)
+
