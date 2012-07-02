904644ba540fbf6dcd47fb5a006d03a64d299fb2
Sune Kirkeby <mig@ibofobi.dk>
2012-07-02 11:40:57 +0200

    Remove `get_` prefix on page functions.

--- src/yoi/pages.py
+++ src/yoi/pages.py
@@ -4,17 +4,17 @@ from flaskext.genshi import render_response
 from yoi.app import app
 
 @app.route('/')
-def get_index():
+def index():
     return render_response('index.html')
 
 @app.route('/home')
-def get_home():
+def home():
     return render_response('home.html')
 
 @app.route('/journal')
-def get_journal():
+def journal():
     return render_response('journal.html')
 
 @app.route('/new-entry')
-def get_new_entry():
+def new_entry():
     return render_response('new-entry.html')
